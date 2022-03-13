dafd7d20fe7d4abd096cb8beb2be12f0bedceaa1
Madas <73985626+Madasish@users.noreply.github.com>
2022-03-13 21:33:20 +0200

    Yeah

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// const splash = require('unsplash');
 const express = require('express');
 const randomFile = require('select-random-file');
 const app = express();
@@ -6,10 +5,9 @@ const app = express();
 // Config:
 const port = 42069
 
-const dir = `${__dirname}/wallpapers`
+const dir = `${__dirname}/images`
 
-//Config end
-
+// The "code"
 app.get('/', (req, res) => {
     randomFile(dir, (err, file) => {res.sendFile(`${dir}/${file}`)})
 })
