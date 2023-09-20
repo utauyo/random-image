35d424629715dcb9f1b7b5f8ff775bf942c04578
Madars Batraks <madarsbatraks@gmail.com>
2023-09-20 22:36:51 +0300

    Updated to not use redundant packages

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express');
-const randomFile = require('select-random-file');
 const app = express();
+const fs = require('fs');
 
 // Config:
 const port = 42069
-
 const dir = `${__dirname}/images`
 
 // The "code"
 app.get('/', (req, res) => {
-    randomFile(dir, (err, file) => {res.sendFile(`${dir}/${file}`)})
+    fs.readdir(dir, (err, files) => {
+        if(err) throw err
+        const file = files[Math.floor(Math.random() * files.length + 1)]
+        res.sendFile(`${dir}/${file}`)
+    })
 })
 
 app.listen(port, () => {
